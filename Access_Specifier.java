95824296371d5f57085c710f1fd431fbbed4d153
vishal05 <singhvishalkr748@gmail.com>
2024-07-14 19:55:21 +0530

    Access Specifier Program

---
+++ Access_Specifier.java
@@ -0,0 +1,23 @@
+package accessspecifier;
+
+public class Access_Specifier {
+public void add() {
+	
+}
+private void sub() {
+	
+}
+protected void div() {
+	
+}
+void mul() {
+	
+}
+public static void main(String[] args) {
+	Access_Specifier as = new Access_Specifier();
+	as.sub();
+as.add();
+as.div();
+as.mul();
+}
+}
